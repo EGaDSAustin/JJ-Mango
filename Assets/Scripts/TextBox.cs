b05def23cb512c62cb71f4773fb19f0120d480ed
Bobby Youstra <bobbyyoustra@protonmail.com>
2023-12-15 19:48:20 -0800

    added text box

--- Assets/Scripts/TextBox.cs
+++ Assets/Scripts/TextBox.cs
@@ -1,13 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextBox : MonoBehaviour
 {
+    public const float CHARACTER_TIME = 0.03f;
+    public TextMeshProUGUI mTextMeshPro;
+
+    private String mCurrentString;
+    private int mCharacterProgress = 0;
+
+    private Coroutine mTextScrollingCoroutine;
+
+    IEnumerator PrintScrolling()
+    {
+        mTextMeshPro.text = "";
+
+        while(mCharacterProgress <= mCurrentString.Length)
+        {
+            // suspend execution for 5 seconds
+            yield return new WaitForSeconds(CHARACTER_TIME);
+
+            mTextMeshPro.text += mCurrentString[mCharacterProgress];
+
+            ++mCharacterProgress;
+        }
+    }
+
+    private void SetText(String newTextString)
+    {
+        mCurrentString = newTextString;
+
+        if(mTextScrollingCoroutine != null)
+        {
+            StopCoroutine(mTextScrollingCoroutine);
+        }
+
+        mTextScrollingCoroutine = StartCoroutine("PrintScrolling");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetText(mTextMeshPro.text);
     }
 
     // Update is called once per frame
